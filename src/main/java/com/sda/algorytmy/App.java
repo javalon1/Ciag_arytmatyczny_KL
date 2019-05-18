c4e3bc2f6fdd9cdd23b7999ac4099d17e113b11d
Kamil Lesinski <kamilesinski@live.co.uk>
2019-05-18 12:24:43 +0100

    final commit

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,5 +1,7 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,6 +11,13 @@ public class App
     public static void main( String[] args )
 
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        int a1 = scanner.nextInt();
+        int d = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        for (int i = 2; i<n; i++){
+            System.out.println(a1 + (i-1)*d);
+        }
     }
 }
